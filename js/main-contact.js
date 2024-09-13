5768ff7e15ce6ee661f5caa2c010d08e919cac84
lehonganh29122002 <lehonganh29122002@gmail.com>
2024-09-13 18:35:25 +0700

    change 1

--- js/main-contact.js
+++ js/main-contact.js
@@ -1,50 +1,48 @@
-/*const url = window.location.href;
-const replacedURL = url.replace('#', '&');
+const url = window.location.href;
+const replacedURL = url.replace("#", "&");
 const finalURL = new URLSearchParams(replacedURL);
-var accessToken = finalURL.get('access_token');
+var accessToken = finalURL.get("access_token");
 var idToken = finalURL.get("id_token");
 var UserName, UserEmail;
 
-AWS.config.region = 'ap-southeast-1';
+AWS.config.region = "ap-southeast-1";
 AWS.config.apiVersions = {
-    cognitoidentityserviceprovider: '2016-04-18'
+  cognitoidentityserviceprovider: "2016-04-18",
 };
 
 var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
 var params = {
-    AccessToken: accessToken
+  AccessToken: accessToken,
 };
 
-cognitoidentityserviceprovider.getUser(params, function(err, data) {
-    if (err) {
-        window.location.href = '';
-    } else {
-        console.log(data);
+cognitoidentityserviceprovider.getUser(params, function (err, data) {
+  if (err) {
+    window.location.href = "https://website.honganh2912.id.vn/";
+  } else {
+    console.log(data);
 
-        for (var i = 0; i < data.UserAttributes.length; i++) {
-            if (data.UserAttributes[i].Name === 'name') {
-                UserName = data.UserAttributes[i].Value;
-            }
-            if (data.UserAttributes[i].Name === 'email') {
-                UserEmail = data.UserAttributes[i].Value;
-            }
-        }
-
-        if (document.getElementById('userNameDisplay')) {
-            document.getElementById('userNameDisplay').innerHTML = UserName;
-        }
-        if (document.getElementById('userEmailDisplay')) {
-            document.getElementById('userEmailDisplay').innerHTML = UserEmail;
-        }
-
-        if (document.getElementById('userNameInput')) {
-            document.getElementById('userNameInput').value = UserName;
-        }
-        if (document.getElementById('userEmailInput')) {
-            document.getElementById('userEmailInput').value = UserEmail;
-        }
+    for (var i = 0; i < data.UserAttributes.length; i++) {
+      if (data.UserAttributes[i].Name === "name") {
+        UserName = data.UserAttributes[i].Value;
+      }
+      if (data.UserAttributes[i].Name === "email") {
+        UserEmail = data.UserAttributes[i].Value;
+      }
     }
+
+    if (document.getElementById("userNameDisplay")) {
+      document.getElementById("userNameDisplay").innerHTML = UserName;
+    }
+    if (document.getElementById("userEmailDisplay")) {
+      document.getElementById("userEmailDisplay").innerHTML = UserEmail;
+    }
+
+    if (document.getElementById("userNameInput")) {
+      document.getElementById("userNameInput").value = UserName;
+    }
+    if (document.getElementById("userEmailInput")) {
+      document.getElementById("userEmailInput").value = UserEmail;
+    }
+  }
 });
-
-
